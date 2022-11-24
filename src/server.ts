5e8bbece7f7fef16aa55c1399ce2ca96d9fc9a12
Junior Sousa <jrsousadev@gmail.com>
2022-11-24 01:20:03 -0300

    added dependencies, configure tsconfig and added prisma

--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import { serverHttp } from "./app";
-import './websocket'
-
-// app.listen(3000, () => console.log("Server is running on PORT 3000"));
-serverHttp.listen(3000, () => console.log("Socket.io is running on PORT 3000"));
