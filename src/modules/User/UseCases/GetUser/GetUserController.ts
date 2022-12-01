f83f56db7989bde1ef299ae21cf96bb1828edeae
Junior Sousa <jrsousadev@gmail.com>
2022-12-01 01:48:38 -0300

    refactoring useCases

--- src/modules/User/UseCases/GetUser/GetUserController.ts
+++ src/modules/User/UseCases/GetUser/GetUserController.ts
@@ -1,17 +1,21 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { container } from "tsyringe";
+import { AppError } from "../../../../shared/errors/AppError";
+import { validObjectId } from "../../../../utils/validObjectId";
 import { GetUserService } from "./GetUserService";
 class GetUserController {
-  async handle(request: Request, response: Response): Promise<any> {
+  async handle(request: Request, response: Response, next: NextFunction): Promise<any> {
     try {
       const { id } = request.params;
+
+      if (!validObjectId(id)) throw new AppError("id invalid", 400);
 
       const getUserService = container.resolve(GetUserService);
       const user = await getUserService.execute({ id });
 
       return response.status(200).json(user);
     } catch (err) {
-      return response.status(400).json({message: "Internal server error"})
+      next(err)
     }
   }
 }
