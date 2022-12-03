3ce7bf5352b805d3a25f02b6826fe1e185115e39
Junior Sousa <jrsousadev@gmail.com>
2022-12-03 14:53:21 -0300

    finishing tests

--- src/app/modules/user/create-user/create-user-factory.ts
+++ src/app/modules/user/create-user/create-user-factory.ts
@@ -1,17 +1,16 @@
-import { UserRepository } from "../../../repositories/user-repository";
 import { UserModule } from "../user-module";
 import { CreateUserController } from "./create-user-controller";
 import { CreateUserUseCase } from "./create-user-usecase";
+import { config } from "../../../../config";
+import { InMemoryUserRepository } from "../../../../../tests/_repositories/in-memory-user-repository";
 
-export const createUserFactory = (
-    InMemoryUserRepository?: UserRepository
-) => {
-  const userRepository = InMemoryUserRepository ?? new UserModule();
-  const createUser = new CreateUserUseCase(
-    userRepository
-  );
-  const createUserController = new CreateUserController(
-    createUser
-  );
+const TEST_ENVIRONMENT = config.ENVIRONMENT === "TEST" ? true : false;
+
+export const createUserFactory = () => {
+  const userRepository = TEST_ENVIRONMENT
+    ? new InMemoryUserRepository()
+    : new UserModule();
+  const createUser = new CreateUserUseCase(userRepository);
+  const createUserController = new CreateUserController(createUser);
   return createUserController;
 };
--- src/app/modules/user/get-user/get-user-factory.ts
+++ src/app/modules/user/get-user/get-user-factory.ts
@@ -1,13 +1,16 @@
-import { UserRepository } from "../../../repositories/user-repository";
 import { UserModule } from "../user-module";
 import { GetUserController } from "./get-user-controller";
 import { GetUserUseCase } from "./get-user-usecase";
+import { config } from "../../../../config";
+import { InMemoryUserRepository } from "../../../../../tests/_repositories/in-memory-user-repository";
+
+const TEST_ENVIRONMENT = config.ENVIRONMENT === "TEST" ? true : false;
 
 export const getUserFactory = () => {
-  const userRepository = new UserModule();
-  const getUser = new GetUserUseCase(
-    userRepository
-  );
+  const userRepository = TEST_ENVIRONMENT
+    ? new InMemoryUserRepository()
+    : new UserModule();
+  const getUser = new GetUserUseCase(userRepository);
   const getUserController = new GetUserController(getUser);
   return getUserController;
 };
