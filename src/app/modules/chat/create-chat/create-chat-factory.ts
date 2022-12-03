0193ea54175cc1c383865fe53e69b5ab291a77fb
Junior Sousa <jrsousadev@gmail.com>
2022-12-03 14:53:02 -0300

    finishing tests

--- src/app/modules/chat/create-chat/create-chat-factory.ts
+++ src/app/modules/chat/create-chat/create-chat-factory.ts
@@ -1,11 +1,21 @@
 import { CreateChatController } from "./create-chat-controller";
 import { ChatModule } from "../chat-module";
-import { CreateChatUseCase } from "./create-chat-usecase"
-import { ChatRepository } from "../../../repositories/chat-repository";
+import { CreateChatUseCase } from "./create-chat-usecase";
+import { InMemoryChatRepository } from "../../../../../tests/_repositories/in-memory-chat-repository";
+import { config } from "../../../../config";
+import { InMemoryUserRepository } from "../../../../../tests/_repositories/in-memory-user-repository";
+import { UserModule } from "../../user/user-module";
 
-export const createChatFactory = (InMemoryChatRepository?: ChatRepository) => {
-    const chatRepository = InMemoryChatRepository ?? new ChatModule();
-    const createChat = new CreateChatUseCase(chatRepository);
-    const createChatController = new CreateChatController(createChat);
-    return createChatController;
-}
+const TEST_ENVIRONMENT = config.ENVIRONMENT === "TEST" ? true : false;
+
+export const createChatFactory = () => {
+  const chatRepository = TEST_ENVIRONMENT
+    ? new InMemoryChatRepository()
+    : new ChatModule();
+  const userRepository = TEST_ENVIRONMENT
+    ? new InMemoryUserRepository()
+    : new UserModule();
+  const createChat = new CreateChatUseCase(chatRepository, userRepository);
+  const createChatController = new CreateChatController(createChat);
+  return createChatController;
+};
--- src/app/modules/chat/create-chat/create-chat-usecase.ts
+++ src/app/modules/chat/create-chat/create-chat-usecase.ts
@@ -1,4 +1,6 @@
 import { ChatRepository } from "../../../repositories/chat-repository";
+import { UserRepository } from "../../../repositories/user-repository";
+import { AppError } from "../../../shared/errors/AppError";
 
 interface CreateChatRequest {
   userOne: string;
@@ -6,10 +8,18 @@ interface CreateChatRequest {
 }
 
 export class CreateChatUseCase {
-  constructor(private chatRepository: ChatRepository) {}
+  constructor(
+    private chatRepository: ChatRepository,
+    private userRepository: UserRepository,
+  ) {}
 
   execute = async ({ userOne, userTwo }: CreateChatRequest) => {
     try {
+      const getUserOne = await this.userRepository.findById(userOne);
+      const getUserTwo = await this.userRepository.findById(userTwo);
+
+      if (!getUserOne || !getUserTwo) throw new AppError("One of the users does not exist");
+
       return await this.chatRepository.create({
         userOne,
         userTwo,
