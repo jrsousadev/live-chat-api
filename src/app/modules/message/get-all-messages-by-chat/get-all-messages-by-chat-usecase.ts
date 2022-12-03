eb9fb1fca5153e1d69ee20ce3132b5cf7c9e646e
Junior Sousa <jrsousadev@gmail.com>
2022-12-03 14:53:17 -0300

    finishing tests

--- src/app/modules/message/get-all-messages-by-chat/get-all-messages-by-chat-usecase.ts
+++ src/app/modules/message/get-all-messages-by-chat/get-all-messages-by-chat-usecase.ts
@@ -1,17 +1,24 @@
+import { ChatRepository } from "../../../repositories/chat-repository";
 import { MessageRepository } from "../../../repositories/message-repository";
 import { AppError } from "../../../shared/errors/AppError";
 import { validObjectId } from "../../../utils/validObjectId";
-
 interface GetAllMessagesByChatRequest {
   chatId: string;
 }
 
 export class GetAllMessagesByChatUseCase {
-  constructor(private messageRepository: MessageRepository) {}
+  constructor(
+    private chatRepository: ChatRepository,
+    private messageRepository: MessageRepository
+  ) {}
 
   execute = async ({ chatId }: GetAllMessagesByChatRequest) => {
     try {
-      if (!validObjectId(chatId)) throw new AppError("id invalid", 400);
+      if (!validObjectId(chatId)) throw new AppError("Invalid Chat ID", 400);
+
+      const chat = await this.chatRepository.findById(chatId);
+
+      if (!chat) throw new AppError("Chat is not exist")
 
       return await this.messageRepository.findAllByChat(chatId);
     } catch (err) {
--- src/app/modules/message/get-last-message-by-chat/get-last-message-by-chat-factory.ts
+++ src/app/modules/message/get-last-message-by-chat/get-last-message-by-chat-factory.ts
@@ -1,13 +1,22 @@
-import { MessageRepository } from "../../../repositories/message-repository";
+import { InMemoryMessageRepository } from "../../../../../tests/_repositories/in-memory-message-repository";
 import { MessageModule } from "../message-module";
 import { GetLastMessageByChatController } from "./get-last-message-by-chat-controller";
 import { GetLastMessageByChatUseCase } from "./get-last-message-by-chat-usecase";
+import { config } from "../../../../config";
+import { InMemoryChatRepository } from "../../../../../tests/_repositories/in-memory-chat-repository";
+import { ChatModule } from "../../chat/chat-module";
 
-export const getLastMessageByChatFactory = (
-  InMemoryMessageRepository?: MessageRepository
-) => {
-  const messageRepository = InMemoryMessageRepository ?? new MessageModule();
+const TEST_ENVIRONMENT = config.ENVIRONMENT === "TEST" ? true : false;
+
+export const getLastMessageByChatFactory = () => {
+  const messageRepository = TEST_ENVIRONMENT
+    ? new InMemoryMessageRepository()
+    : new MessageModule();
+  const chatRepository = TEST_ENVIRONMENT
+    ? new InMemoryChatRepository()
+    : new ChatModule();
   const getLastMessageByChat = new GetLastMessageByChatUseCase(
+    chatRepository,
     messageRepository
   );
   const getLastMessageByChatController = new GetLastMessageByChatController(
